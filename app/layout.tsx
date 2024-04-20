6e60e76bd377af2cb94ca79e5af0491170089aa3
Renaldo Menelas <renaldomenelas@Renaldos-Air.home>
2024-04-19 21:47:29 -0400

    Co_space first commit

---
+++ app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import "./globals.css";
+import Navbar from './components/navbar/Navbar'
+const inter = Nunito({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "CoSpace",
+  description: "Sharing your workspace",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Navbar/>
+        {children}</body>
+    </html>
+  );
+}
