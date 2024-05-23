e013402af260b09f43f2e96a8eea415ada1c0777
Renaldo Menelas <renaldomenelas@Renaldos-Air.home>
2024-05-23 18:29:08 -0400

    log in functionality

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -1,21 +1,28 @@
 'use client';
 
 import axios from 'axios';
+import {signIn} from 'next-auth/react'
 import { AiFillGithub } from 'react-icons/ai';
 import { FcGoogle } from 'react-icons/fc';
 import { useCallback, useState } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import useRegisterModal from '@/app/hooks/useRegisterModal';
+import useLoginModal from '@/app/hooks/useLoginModal';
+
 import { error } from 'console';
 import Modal from './Modal';
 import Heading from '../Heading';
 import Input from '../inputs/Input';
 import toast from 'react-hot-toast';
 import Button from '../Button';
+import {useRouter} from 'next/navigation'
+
 
 
 const LoginModal = () => {
+  const router = useRouter()
   const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
   const [isLoading, setIsLoading] = useState(false);
 
   const{
@@ -26,7 +33,6 @@ const LoginModal = () => {
     }
   } = useForm<FieldValues>({
     defaultValues: {
-      name: '',
       email: '',
       password: ''
     }
@@ -34,37 +40,37 @@ const LoginModal = () => {
 
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     setIsLoading(true);
-
-    axios.post('/api/register', data)
-      .then(() => {
-        registerModal.onClose();
-      })
-      .catch((error) => {
-        toast.error('Oops');
-      })
-      .finally(() => {
+     
+    signIn('credentials', {
+      ...data,
+      redirect:false,
+    }).then((callback) => {
+      setIsLoading(false)
+      console.log(callback)
+      if (callback?.ok) {
+        toast.success('Logged in');
+        router.refresh();
+        loginModal.onClose()
+      }
+      if (callback?.error) {
+        toast.error(callback.error)
+      }}).catch((error) => {
         setIsLoading(false);
-      })
+        console.error(error); // Log the error for debugging
+        toast.error('An error occurred during sign-in.');
+    })
   }
 
   const bodyContent =(
     <div className="flex flex-col gap-4">
       <Heading 
-        title="Welcome to CoSpace"
+        title="Welcome back"
         subtitle="Create an account!"
         center
       />
       <Input 
       id="email"
       label="Email"
-      disabled={isLoading}
-      register={register}
-      errors={errors}
-      required
-      />
-       <Input 
-      id="name"
-      label="Name"
       disabled={isLoading}
       register={register}
       errors={errors}
@@ -126,10 +132,10 @@ const LoginModal = () => {
   return (
     <Modal
       disabled={isLoading}
-      isOpen={registerModal.isOpen}
-      title="Register"
+      isOpen={loginModal.isOpen}
+      title="Login"
       actionLabel='Continue'
-      onClose={registerModal.onClose}
+      onClose={loginModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
       body={bodyContent}
       footer={footerContent}
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,9 @@ import { Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from './components/navbar/Navbar'
 import RegisterModal from "./components/modals/RegisterModal";
+import LoginModal from "./components/modals/LoginModal";
 import ToasterProvider from "./providers/ToasterProvider";
+import getCurrentUser from "./actions/getCurrentUser";
 const inter = Nunito({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -11,17 +13,19 @@ export const metadata: Metadata = {
   description: "Sharing your workspace",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const currentUser = await getCurrentUser()
   return (
     <html lang="en">
       <body className={inter.className}>
         <ToasterProvider />
+        <LoginModal />
         <RegisterModal />
-        <Navbar />
+        <Navbar currentUser={currentUser}/>
         {children}
       </body>
     </html>
