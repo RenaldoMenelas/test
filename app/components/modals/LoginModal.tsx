eb1250c2db512c41410e613855ab492146b02e4d
Renaldo Menelas <renaldomenelas@Renaldos-Air.home>
2024-05-29 13:40:26 -0400

    clearing node modules

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -9,7 +9,6 @@ import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import useRegisterModal from '@/app/hooks/useRegisterModal';
 import useLoginModal from '@/app/hooks/useLoginModal';
 
-import { error } from 'console';
 import Modal from './Modal';
 import Heading from '../Heading';
 import Input from '../inputs/Input';
