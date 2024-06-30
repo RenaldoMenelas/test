b85074027acb38cfcdb824d29dc3b68bd565cef3
PresTroph <prestigeguedegbe@gmail.com>
2024-06-30 09:44:33 -0400

    made next.config.mjs take in images, nextauth.ts fixed to handle errors, register login modal and modal.tsx got some minor styling. Most importantly you can log in with which ever provider you want, just once you sigh up with one and it is using the same email such as if google and github use the same email to log in i made it so you can only sign up and log in with one not both. you can use as many accounts as you want as long as the emails aren't replicated if so you'll be thrown an error.

--- app/components/modals/RegisterModal.tsx
+++ app/components/modals/RegisterModal.tsx
@@ -3,29 +3,29 @@
 import axios from 'axios';
 import { AiFillGithub } from 'react-icons/ai';
 import { FcGoogle } from 'react-icons/fc';
-import { useCallback, useState } from 'react';
+import { useState } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import useRegisterModal from '@/app/hooks/useRegisterModal';
-import { error } from 'console';
+import useLoginModal from '@/app/hooks/useLoginModal';
 import Modal from './Modal';
 import Heading from '../Heading';
 import Input from '../inputs/Input';
 import toast from 'react-hot-toast';
 import Button from '../Button';
-import { sign } from 'crypto';
 import { signIn } from 'next-auth/react';
-
+import { useRouter } from 'next/navigation';
 
 const RegisterModal = () => {
+  const router = useRouter();
   const registerModal = useRegisterModal();
+  const loginModal = useLoginModal();
   const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
 
-  const{
+  const {
     register,
     handleSubmit,
-    formState: {
-      errors,
-    }
+    formState: { errors }
   } = useForm<FieldValues>({
     defaultValues: {
       name: '',
@@ -42,96 +42,96 @@ const RegisterModal = () => {
         registerModal.onClose();
       })
       .catch((error) => {
-        toast.error('Oops');
-      })
-      .finally(() => {
+        setErrorMessage('A user with this email already exists.');
         setIsLoading(false);
-      })
-  }
+      });
+  };
 
-  const bodyContent =(
+  const bodyContent = (
     <div className="flex flex-col gap-4">
-      <Heading 
+      <Heading
         title="Welcome to CoSpace"
         subtitle="Create an account!"
         center
       />
-      <Input 
-      id="email"
-      label="Email"
-      disabled={isLoading}
-      register={register}
-      errors={errors}
-      required
+      <Input
+        id="email"
+        label="Email"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
       />
-       <Input 
-      id="name"
-      label="Name"
-      disabled={isLoading}
-      register={register}
-      errors={errors}
-      required
+      <Input
+        id="name"
+        label="Name"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
       />
-       <Input 
-      id="password"
-      label="Password"
-      disabled={isLoading}
-      register={register}
-      errors={errors}
-      required
+      <Input
+        id="password"
+        label="Password"
+        disabled={isLoading}
+        register={register}
+        errors={errors}
+        required
       />
+      {errorMessage && (
+        <div className="text-red-500 text-center mt-2">
+          {errorMessage}
+        </div>
+      )}
     </div>
   );
 
   const footerContent = (
     <div className="flex flex-col gap-4 mt-3">
       <hr />
-      <Button 
+      <Button
         outline
         label="Continue with Google"
         icon={FcGoogle}
-        onClick={() => signIn('google')}
+        onClick={() => signIn('google', { callbackUrl: window.location.href })}
       />
-     
-      <div
-      className='
-      text-neutral-500
-      text-center
-      mt-4
-      font-light
-      '
-      >
+      <Button
+        outline
+        label="Continue with GitHub"
+        icon={AiFillGithub}
+        onClick={() => signIn('github', { callbackUrl: window.location.href })}
+      />
+      <div className="text-neutral-500 text-center mt-4 font-light">
         <div className="justify-center flex flex-row items-center gap-2">
           <div>
             Already have an account?
           </div>
           <div
-          onClick={registerModal.onClose}
-            className="
-            text-neutral-800
-            cursor-pointer
-            hover:underline
-            "
+            onClick={() => {
+              registerModal.onClose();
+              loginModal.onOpen();
+            }}
+            className="text-neutral-800 cursor-pointer hover:underline"
           >
             Log in
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 
   return (
     <Modal
       disabled={isLoading}
       isOpen={registerModal.isOpen}
       title="Register"
-      actionLabel='Continue'
+      actionLabel="Continue"
       onClose={registerModal.onClose}
       onSubmit={handleSubmit(onSubmit)}
       body={bodyContent}
       footer={footerContent}
     />
-  )
-}
+  );
+};
 
 export default RegisterModal;
