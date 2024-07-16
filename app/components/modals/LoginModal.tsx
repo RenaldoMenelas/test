a96160d841cc092c369a3fbba80c1fcba00eb590
PresTroph <prestigeguedegbe@gmail.com>
2024-07-15 20:54:57 -0400

    used npm run lint to make sure there were no error there was one in the LoginModal from Don't have an account to Donapos;t have an account. IDK why. but no more errors

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -105,7 +105,7 @@ const LoginModal = () => {
       <div className="text-neutral-500 text-center mt-4 font-light">
         <div className="justify-center flex flex-row items-center gap-2">
           <div>
-            Don't have an account?
+            Don&apos;t have an account?
           </div>
           <div
             onClick={() => {
