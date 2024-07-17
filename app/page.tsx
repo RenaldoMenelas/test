52681b852a96b3e1d87025fdddfaf906dfb8860a
Renaldo Menelas <renaldomenelas@Renaldos-Air.home>
2024-07-17 15:37:33 -0400

    revised page

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
-//import SideBar from './components/sidebar/SideBar'
+// Ensure this file is located in the `app` directory, e.g., `app/page.tsx`
+
 import React from 'react';
 import getCurrentUser from './actions/getCurrentUser';
 import getListings, { IListingsParams } from './actions/getListings';
@@ -9,50 +10,54 @@ import EmptyState from './components/EmptyState';
 import ListingCard from './components/listings/ListingCard';
 
 interface HomeProps {
-  searchParams: IListingsParams
+  searchParams: IListingsParams;
 }
 
-const Home = async ({searchParams}: HomeProps ) => {
+// Function to fetch data
+async function fetchData(searchParams: IListingsParams) {
   const listings = await getListings(searchParams);
   const currentUser = await getCurrentUser();
+  return { listings, currentUser };
+}
+
+const Home = async ({ searchParams }: HomeProps) => {
+  const { listings, currentUser } = await fetchData(searchParams);
 
   if (listings.length === 0) {
     return (
       <ClientOnly>
         <EmptyState showReset />
       </ClientOnly>
-    )
+    );
   }
-
 
   return (
     <ClientOnly>
       <Container>
-        <div className="
-        pt-24
-        grid
-        grid-cols-1
-        sm:grid-cols-2
-        md:grid-cols-3
-        lg:grid-cols-4
-        xl:grid-cols-5
-        2xl:grid-cols-6
-        gap-8
-        "        
+        <div
+          className="
+          pt-24
+          grid
+          grid-cols-1
+          sm:grid-cols-2
+          md:grid-cols-3
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6
+          gap-8
+        "
         >
-          {listings.map((listing) => {
-            return (
-              <ListingCard
+          {listings.map((listing) => (
+            <ListingCard
               currentUser={currentUser}
               key={listing.id}
               data={listing}
-              />
-            )
-          })}
+            />
+          ))}
         </div>
       </Container>
     </ClientOnly>
   );
-}
+};
 
 export default Home;
